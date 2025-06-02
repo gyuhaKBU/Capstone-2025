81cf926f86eac6f4bca7d4794138c178e1acaa76
Gyuha <offline3903@gmail.com>
2025-06-02 09:03:21 +0900

    특정 조건에서만 AWS로  데이터 보내게

--- arduino/start/start.ino
+++ arduino/start/start.ino
@@ -1,7 +1,8 @@
 /* ------------------------------------------------- 전처리기 ------------------------------------------------- */
 // 보드 고유 ID
-#define PATIENT_ID   "p1001"
+#define PATIENT_ID   "p1002"
 #define RASPI_ID     "pi0001"
+#define INST_ID     "inst001"
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -12,11 +13,11 @@
 #define BTN_PIN  D5   // GPIO14 | 푸시버튼
 
 /* ------------------------------------------------- 상수 ------------------------------------------------- */
-const char* GATEWAY_STATUS_TOPIC = "gateway/" RASPI_ID "/status";  // 라즈베리파이 상태 토픽
+const char* GATEWAY_STATUS_TOPIC = "gateway/" INST_ID "-" RASPI_ID "/status";  // 라즈베리파이 상태 토픽
 
 /* ------------------------------------------------- 전역 변수 ------------------------------------------------- */
 // WiFi 설정
-const char* ssid        = RASPI_ID;
+const char* ssid = INST_ID "-" RASPI_ID;
 const char* password    = "12345678";
 const char* mqtt_server = "192.168.4.1";
 
@@ -29,26 +30,40 @@ char topicSensor[32];
 char topicAck[32];
 
 // 멀티태스킹
-unsigned long previous_button_debounce = 0;
-unsigned long previous_readSendSensor  = 0;
-const unsigned long cycle_button_debounce = 15;    // 디바운싱 샘플 주기 (ms)
-const unsigned long cycle_readSendSensor  = 5000;  // 센서 읽고 전송 주기 (ms)
+// unsigned long previous_button_debounce = 0;
+unsigned long previous_sendSensor  = 0;
+unsigned long previous_readSensor  = 0;
 
-// 버튼 디바운스 상태
-bool lastButtonReading = HIGH;
-bool buttonState       = HIGH;
-bool btn_pressed       = false;
-unsigned long lastDebounceTime = 0;
+// const unsigned long cycle_button_debounce = 15;    // 디바운싱 샘플 주기 (ms)
+const unsigned long cycle_readSensor  = 500;  // 센서 읽기 주기 (ms)
+const unsigned long cycle_sendSensor  = 5000;  // 센서 읽고 전송 주기 (ms)
+
+// 버튼 인터럽트
+volatile bool btn_pressed = false;
+volatile unsigned long lastDebounce = 0;
+const unsigned long DEBOUNCE_MS = 50;
+// bool lastButtonReading = HIGH;
+// bool buttonState       = HIGH;
+// bool btn_pressed       = false;
+// unsigned long lastDebounceTime = 0;
+
+//초음파 센서 데이터 전송 판단
+bool ultraSonic_upOrDown = false;
+const int ultraSonic_HIGH_TH = 205;
+const int ultraSonic_LOW_TH  = 195;
 
 /* ------------------------------------------------- 함수 선언 ------------------------------------------------- */
 void setup_wifi();
 void reconnect();
 void callback(char* topic, byte* payload, unsigned int length);
+void IRAM_ATTR onButtonPressed(); //버튼 인터럽트
 long readUltrasonicDistance();
 
 /* ------------------------------------------------- 셋업 함수 ------------------------------------------------- */
 void setup() {
   Serial.begin(115200);
+
+  randomSeed(analogRead(A0)); //부팅마다 다른 시드로 동작
 
   // WiFi 연결
   setup_wifi();
@@ -74,6 +89,11 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(BTN_PIN, INPUT_PULLUP);
+
+  //버튼 인터럽트
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN),
+                  onButtonPressed,
+                  FALLING);
 }
 
 /* ------------------------------------------------- 루프 함수 ------------------------------------------------- */
@@ -86,55 +106,69 @@ void loop() {
   }
   client.loop();
 
-  // 버튼 디바운싱
-  if (currentMillis - previous_button_debounce >= cycle_button_debounce) {
-    previous_button_debounce = currentMillis;
-    bool reading = digitalRead(BTN_PIN);
-    if (reading != lastButtonReading) {
-      lastDebounceTime = currentMillis;
-    }
-    lastButtonReading = reading;
-    if (currentMillis - lastDebounceTime > cycle_button_debounce) {
-      if (reading != buttonState) {
-        buttonState = reading;
-        if (buttonState == LOW) {
-          btn_pressed = true;
-        }
-      }
-    }
-  }
+  // // 버튼 디바운싱
+  // if (!btn_pressed && (currentMillis - previous_button_debounce >= cycle_button_debounce)) {
+  //   previous_button_debounce = currentMillis;
+  //   bool reading = digitalRead(BTN_PIN);
+  //   if (reading != lastButtonReading) {
+  //     lastDebounceTime = currentMillis;
+  //   }
+  //   lastButtonReading = reading;
+  //   if (currentMillis - lastDebounceTime > cycle_button_debounce) {
+  //     if (reading != buttonState) {
+  //       buttonState = reading;
+  //       if (buttonState == LOW) {
+  //         btn_pressed = true;
+  //       }
+  //     }
+  //   }
+  // }
 
-  // 게이트웨이 온라인 상태에서만 발행
-  if (gatewayAlive && (currentMillis - previous_readSendSensor >= cycle_readSendSensor)) {
-    previous_readSendSensor = currentMillis;
+  if (currentMillis - previous_readSensor >= cycle_readSensor) {
+    previous_readSensor = currentMillis;
 
     // 센서값 읽기
-    int call        = btn_pressed ? 1 : 0;
-    btn_pressed     = false;
-    int fall        = random(0, 2);
+    int fall        = (random(1000) == 0) ? 1 : 0;
     long ultraSonic = readUltrasonicDistance();
+    int call        = btn_pressed ? 1 : 0;
 
-    // JSON 패킹
-    StaticJsonDocument<128> doc;
-    doc["call"]       = call;
-    doc["fall"]       = fall;
-    doc["ultraSonic"] = ultraSonic;
-    char buffer[128];
-    serializeJson(doc, buffer);
+    bool ultraSonic_nowAbove = ultraSonic >= ultraSonic_HIGH_TH ? true
+                              : ultraSonic <= ultraSonic_LOW_TH  ? false
+                              : ultraSonic_upOrDown;
+    bool ultraSonic_crossed = (ultraSonic_nowAbove != ultraSonic_upOrDown);
+      
+    // 게이트웨이 온라인 상태에서만 발행
+    if(call || fall || ultraSonic_crossed){
+      if (gatewayAlive && (currentMillis - previous_sendSensor >= cycle_sendSensor)) {
+        previous_sendSensor = currentMillis;
+        ultraSonic_upOrDown    = ultraSonic_nowAbove;  // 초음파 전송 상태 업데이트
+        if (btn_pressed) {
+          btn_pressed = false;
+        }
 
-    // 발행
-    bool ok = client.publish(topicSensor, buffer);
-    if (!ok) {
-      Serial.println(F("Publish failed"));
-    } else {
-      Serial.print(F("[발행] ")); Serial.println(buffer);
+        // JSON 패킹
+        StaticJsonDocument<128> doc;
+        doc["call"]       = (call != 0);
+        doc["fall"]       = (fall != 0);
+        doc["ultraSonic"] = ultraSonic;
+        char buffer[128];
+        serializeJson(doc, buffer);
+
+
+        // 발행
+        bool ok = client.publish(topicSensor, buffer);
+        if (!ok)
+          Serial.println(F("Publish failed"));
+        else
+          Serial.print(F("[발행] ")); Serial.println(buffer);
+      }
+      else if (!gatewayAlive && (currentMillis - previous_sendSensor >= cycle_sendSensor)) {
+        // 게이트웨이 오프라인 상태 알림 (로그)
+        Serial.println(F("[경고] 게이트웨이 오프라인, 발행 중단"));
+        // 타이머 갱신으로 지속 경고 방지
+        previous_sendSensor = currentMillis;
+      }
     }
-  }
-  else if (!gatewayAlive && (currentMillis - previous_readSendSensor >= cycle_readSendSensor)) {
-    // 게이트웨이 오프라인 상태 알림 (로그)
-    Serial.println(F("[경고] 게이트웨이 오프라인, 발행 중단"));
-    // 타이머 갱신으로 지속 경고 방지
-    previous_readSendSensor = currentMillis;
   }
 }
 
@@ -184,6 +218,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
       gatewayAlive = false;
       Serial.println(F("[상태] 게이트웨이 오프라인"));
     }
+  }
+}
+
+void IRAM_ATTR onButtonPressed() {
+  unsigned long now = millis();
+  if (now - lastDebounce > DEBOUNCE_MS) {
+    btn_pressed  = true;
+    lastDebounce = now;
   }
 }
 
